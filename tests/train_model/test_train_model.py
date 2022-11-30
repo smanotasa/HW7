c2ab15014e371972ce2262e198d74624441de3de
CatalinaOdizzio <catalina.odizzio@bse.eu>
2022-11-30 17:07:17 +0100

    change test train model

--- tests/train_model/test_train_model.py
+++ tests/train_model/test_train_model.py
@@ -1,2 +1,14 @@
 from cruasan.train_model.train_model import train_model
+from sklearn.linear_model import LogisticRegression
+import unittest
+import pandas as pd
 
+
+class Test_train(unittest.TestCase):
+    
+    def test_train_model(self):
+        x = pd.DataFrame({'col1': [1,2,4,5,6] , 'col2': [1,2,3,4,5]})
+        y = pd.DataFrame({'col3': [1,0,0,1,0]})
+        output = train_model(x,y)
+        expected_output = LogisticRegression().fit(x, y)
+        self.assertEqual(output,expected_output)
